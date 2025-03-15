7fc4652f9ab3e74014814250d50027b4a3d4bda2
Ernie <ersaavedra.dev@gmail.com>
2025-03-15 11:35:39 -0400

    theme challenge meta
    meta tag fixes for theme challenge

--- projects/theme-challenge/head.php
+++ projects/theme-challenge/head.php
@@ -3,8 +3,8 @@
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
 	<title>Super theme challenge</title>
-	<meta name='description' content='[[insert description]]'>
-	<meta property='og:image' content='images/default.png'>
+	<meta name='description' content='Theme Challenge!'>
+	<meta property='og:image' content='images/default-meta.png'>
 
 	<link rel='stylesheet' href='css/style.css'>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
