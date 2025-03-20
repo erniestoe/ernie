a9c7e02e351407426dd0d7afefd510ce6b801cf1
Ernie <ersaavedra.dev@gmail.com>
2025-03-20 15:40:59 -0400

    header and image fix

--- projects/soccer-theme/head.php
+++ projects/soccer-theme/head.php
@@ -3,8 +3,9 @@
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
 	<title>Super theme challenge</title>
-	<meta name='description' content='[[insert description]]'>
-	<meta property='og:image' content='images/default.png'>
+	<meta property='og:title' content='This is a Soccer Theme!'>
+	<meta name='description' content='Soccer Theme'>
+	<meta property='og:image' content='https://res.cloudinary.com/dhgciqwbz/image/upload/v1742498199/iron-city-goal_aijeaa.png'>
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
