d369f7f13d2c07f1f5e70914911fc7955ae6ac4b
Ernie <ersaavedra.dev@gmail.com>
2025-04-04 09:19:48 -0400

    fix

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
-include ('includes/router.php'); // Handles routing logic
-$currentPage = getCurrentPage(); // Get info for current page (name and file path) -- router function
-
 include ('functions/index.php'); // Loads core functions
 
 checkServer(); //Checks which server site is currently on
+
+include ('includes/router.php'); // Handles routing logic
+$currentPage = getCurrentPage(); // Get info for current page (name and file path) -- router function
 
 include ('includes/header.php');
 include $currentPage['file']; 
