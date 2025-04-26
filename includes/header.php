b86f2914981518d19b3671abe891e31b45694740
Ernie <ersaavedra.dev@gmail.com>
2025-04-26 10:49:16 -0400

    changed design work to visual design

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,7 @@ $pageTitle = renderPageTitle($currentPage['name']);
 					
 					<nav>
 						<a href="index.php?page=garden">Layout Garden</a>
-						<a href="index.php?page=design">Design Work</a>
+						<a href="index.php?page=design">Visual Design</a>
 						<!-- <a target="_blank" href="https://codepen.io/erivera-s">CodePen</a> -->
 
 						<!-- <a href="index.php?page=programming">E4P</a> -->
