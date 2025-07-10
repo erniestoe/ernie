d69b175facc4d3b1b06d9a1d6ca26315722f09aa
Ernie <ersaavedra.dev@gmail.com>
2025-07-10 17:44:50 -0400

    case study work

--- pages/case-study.php
+++ pages/case-study.php
@@ -37,6 +37,12 @@ if ($projectId) {
 				<p><?= $project['caseStudy'] ?></p>
 			</div>
 
+			<?php if ($project['video']): ?>
+				<div class="video">
+					<iframe width="1200" height="600" src="<?= $project['video'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				</div>
+			<?php endif; ?>
+
 			<links>
 				<div class="live">
 					<h2 class="strong-voice">Live sites</h2>
@@ -53,20 +59,22 @@ if ($projectId) {
 					</ul>
 				</div>
 
-				<div class="github">
-					<h2 class="strong-voice">Github</h2>
+				<?php if ($project['githubLinks']): ?>
+					<div class="github">
+						<h2 class="strong-voice">Github</h2>
 
-					<ul>
-						<?php foreach ($project['githubLinks'] as $githubLink):?>
-							<li>
-								<a target="_blank" href="<?=$githubLink['href']?>"><?=$githubLink['text']?></a>
+						<ul>
+							<?php foreach ($project['githubLinks'] as $githubLink):?>
+								<li>
+									<a target="_blank" href="<?=$githubLink['href']?>"><?=$githubLink['text']?></a>
 
-								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></svg>
-							</li>
-						<?php endforeach; ?>
-					</ul>
+									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" viewBox="0 0 256 256"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></svg>
+								</li>
+							<?php endforeach; ?>
+						</ul>
 
-				</div>
+					</div>
+				<?php endif; ?>
 			</links>
 
 			<case-study-gallery class="main-subgrid">
