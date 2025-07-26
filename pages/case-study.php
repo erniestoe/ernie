f78ee7c6bfe7dc6d6d31ba4eaf141884cd1d966d
Ernie <ersaavedra.dev@gmail.com>
2025-07-25 22:20:53 -0400

    case study iframe fix
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- pages/case-study.php
+++ pages/case-study.php
@@ -40,12 +40,12 @@ if ($projectId) {
 			<case-study-gallery class="main-subgrid">
 				<?php if ($project['figma']): ?>
 					<?php foreach ($project['figma'] as $figmaEmbed): ?>
-						<iframe loading="lazy" style="border: 1px solid rgba(0, 0, 0, 0.1);"  src="<?= $figmaEmbed['src']; ?>" allowfullscreen></iframe>
+						<iframe width="100%" loading="lazy" style="border: 1px solid rgba(0, 0, 0, 0.1);"  src="<?= $figmaEmbed['src']; ?>" allowfullscreen></iframe>
 					<?php endforeach ?>
 				<?php endif; ?>
 				
 				<?php if ($project['youtube']): ?>
-					<iframe loading="lazy" src="<?= $project['youtube'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+					<iframe width="100%" loading="lazy" src="<?= $project['youtube'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<?php endif; ?>
 
 				<?php foreach ($project['gallery']  as $image): ?>
