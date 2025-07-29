0eea117e39554a629a164e39c815b76e4cc7b150
Ernie <ersaavedra.dev@gmail.com>
2025-07-29 10:36:36 -0400

    crft case study work
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- pages/case-study.php
+++ pages/case-study.php
@@ -39,16 +39,6 @@
 			</div>
 
 			<case-study-gallery>
-				<?php if ($project['figma']): ?>
-					<?php foreach ($project['figma'] as $figmaEmbed): ?>
-						<iframe width="100%" loading="lazy" style="border: 1px solid rgba(0, 0, 0, 0.1);"  src="<?= $figmaEmbed['src']; ?>" allowfullscreen></iframe>
-					<?php endforeach ?>
-				<?php endif; ?>
-				
-				<?php if ($project['youtube']): ?>
-					<iframe width="100%" loading="lazy" src="<?= $project['youtube'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-				<?php endif; ?>
-
 				<?php foreach ($project['gallery']  as $image): ?>
 					<figure>
 						<a data-fslightbox="gallery" href="<?= $image['src']; ?>">
@@ -62,6 +52,16 @@
 						<?php endif;?>
 					</figure>
 				<?php endforeach; ?>
+
+				<?php if ($project['figma']): ?>
+					<?php foreach ($project['figma'] as $figmaEmbed): ?>
+						<iframe width="100%" loading="lazy" style="border: 1px solid rgba(0, 0, 0, 0.1);"  src="<?= $figmaEmbed['src']; ?>" allowfullscreen></iframe>
+					<?php endforeach ?>
+				<?php endif; ?>
+				
+				<?php if ($project['youtube']): ?>
+					<iframe width="100%" loading="lazy" src="<?= $project['youtube'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				<?php endif; ?>
 			</case-study-gallery>
 
 			<links>
