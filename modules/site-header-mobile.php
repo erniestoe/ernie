5585332ec0a374c8606ddf008f772e287890317c
Ernie <ersaavedra.dev@gmail.com>
2025-08-11 12:24:24 -0400

    added mobile link to blog
    whoops
    
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- modules/site-header-mobile.php
+++ modules/site-header-mobile.php
@@ -22,7 +22,7 @@
 					<a class="loud-voice" href="<?= ENV === 'production'? '/design' : '?page=design'?>">Playground</a>
 				</li>
 				<li>
-					<a class="loud-voice" href="">Blog</a>
+					<a class="loud-voice" target="_blank" href="https://lapanaderia.substack.com/">Blog</a>
 					<svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#f5f5f5" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg>
 				</li>
 			</ul>
