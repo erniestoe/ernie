ac24f9b0e916370ecc0e73b465f72e7bd04e5812
Ernie <ersaavedra.dev@gmail.com>
2025-08-16 12:51:38 -0400

    404 page update
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,6 @@
 <section class="not-found">
 	<inner-column>
 		<h1 class="loud-voice">Whoops... Looks like this page doesn't exist!</h1>
-		<h2 class="attention-voice"><a href="<?=BASE_URL;?>/index.php">Head back home?</a></h2>
+		<h2 class="attention-voice"><a href="<?= ENV === 'production'? '/' : '?page=home'?>">Head back home?</a></h2>
 	</inner-column>
 </section>
