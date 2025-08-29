522426ae8f62d2f537894671bf3e2badfb2477fe
Ernie <ersaavedra.dev@gmail.com>
2025-08-29 14:47:10 -0400

    site updates
    mainly case study stuff.. again
    
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- modules/work-cards-home.php
+++ modules/work-cards-home.php
@@ -22,6 +22,27 @@
 		</div>
 	</work-card>
 
+	<work-card>
+		<a href="<?= ENV === 'production'? '/case-study/crft' : '?page=case-study&slug=crft'?>">
+				<video				
+				      muted
+				      playsinline
+				      loop
+				      preload="metadata"
+				      style="display:block;width:100%;height:auto;"
+				      src="https://res.cloudinary.com/dhgciqwbz/video/upload/v1756483611/crft-teaser_dzjxbw.mp4"
+				      class="video"
+				      poster="https://res.cloudinary.com/dhgciqwbz/image/upload/v1756483744/CleanShot_2025-08-29_at_12.08.24_2x_crmfwh.png"
+				    ></video>
+		</a>
+
+		<div class="text">
+			<h4 class="quiet-voice bold">CRFT</h4>
+
+			<button class="video-control">PLAY</button>
+		</div>
+	</work-card>
+
 	<work-card >
 		<a href="<?= ENV === 'production'? '/case-study/openresource
 		' : '?page=case-study&slug=openresource'?>">
@@ -61,27 +82,6 @@
 
 		<div class="text">
 			<h4 class="quiet-voice bold">Studio Ernie</h4>
-
-			<button class="video-control">PLAY</button>
-		</div>
-	</work-card>
-
-	<work-card>
-		<a href="<?= ENV === 'production'? '/case-study/crft' : '?page=case-study&slug=crft'?>">
-				<video				
-				      muted
-				      playsinline
-				      loop
-				      preload="metadata"
-				      style="display:block;width:100%;height:auto;"
-				      src="https://res.cloudinary.com/dhgciqwbz/video/upload/v1756483611/crft-teaser_dzjxbw.mp4"
-				      class="video"
-				      poster="https://res.cloudinary.com/dhgciqwbz/image/upload/v1756483744/CleanShot_2025-08-29_at_12.08.24_2x_crmfwh.png"
-				    ></video>
-		</a>
-
-		<div class="text">
-			<h4 class="quiet-voice bold">CRFT</h4>
 
 			<button class="video-control">PLAY</button>
 		</div>
