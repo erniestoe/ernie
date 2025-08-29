8996a907929bf5a85fcfa25f95651fe8d6be1912
Ernie <ersaavedra.dev@gmail.com>
2025-08-29 17:33:25 -0400

    site updates
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- modules/work-cards-home.php
+++ modules/work-cards-home.php
@@ -44,28 +44,6 @@
 	</work-card>
 
 	<work-card>
-		<a href="<?= ENV === 'production'? '/case-study/studio-ernie
-		' : '?page=case-study&slug=studio-ernie'?>">
-			<video
-				      muted
-				      playsinline
-				      loop
-				      preload="metadata"
-				      style="display:block;width:100%;height:auto;"
-				      src="https://res.cloudinary.com/dhgciqwbz/video/upload/v1755007792/CleanShot_2025-08-12_at_10.09.17_qt0knf.mov"
-				      class="video"
-				      poster="https://res.cloudinary.com/dhgciqwbz/image/upload/v1755010137/CleanShot_2025-08-12_at_10.47.58_2x_z0f1gq.png"
-				    ></video>
-		</a>
-
-		<div class="text">
-			<h4 class="quiet-voice bold">Studio Ernie</h4>
-
-			<button class="video-control">PLAY</button>
-		</div>
-	</work-card>
-
-	<work-card>
 		<a href="<?= ENV === 'production'? '/case-study/compas' : '?page=case-study&slug=compas'?>">
 				<video				
 				      muted
@@ -85,6 +63,28 @@
 			<button class="video-control">PLAY</button>
 		</div>
 	</work-card>
+
+	<!-- <work-card>
+		<a href="<?= ENV === 'production'? '/case-study/studio-ernie
+		' : '?page=case-study&slug=studio-ernie'?>">
+			<video
+				      muted
+				      playsinline
+				      loop
+				      preload="metadata"
+				      style="display:block;width:100%;height:auto;"
+				      src="https://res.cloudinary.com/dhgciqwbz/video/upload/v1755007792/CleanShot_2025-08-12_at_10.09.17_qt0knf.mov"
+				      class="video"
+				      poster="https://res.cloudinary.com/dhgciqwbz/image/upload/v1755010137/CleanShot_2025-08-12_at_10.47.58_2x_z0f1gq.png"
+				    ></video>
+		</a>
+
+		<div class="text">
+			<h4 class="quiet-voice bold">Studio Ernie</h4>
+
+			<button class="video-control">PLAY</button>
+		</div>
+	</work-card> -->
 
 	<work-card >
 		<a href="<?= ENV === 'production'? '/case-study/openresource
