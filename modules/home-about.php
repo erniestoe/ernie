3e156348170f703c99a2d2dc67242adc22add8cc
Ernie <ersaavedra.dev@gmail.com>
2025-09-02 10:26:21 -0400

    site updates
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- modules/home-about.php
+++ modules/home-about.php
@@ -1,14 +1,8 @@
 <div class="about">
-	<h3 class="attention-voice">What you get from me.</h3>
+	<h3 class="loud-voice">Less noise, <br> more clarity</h3>
 
-	<div data-tilt data-tilt-full-page-listening>
-		<svg class="morph" viewBox="0 0 200 200">
-		  <path id="shape" d="M100,50 A50,50 0 1,0 100.01,50 Z"></path>
-		</svg>
-	</div>
+	<h4 class="calm-voice"><em>is my personal ethos.</em></h4>
 
-	<h4 class="loud-voice">Less noise, <br> more clarity.</h4>
-
-	<p class="strong-voice">
+	<p>
 		I like to morph scattered ideas into clear systems and designs that <em>just work</em>. With a background in Computer Science and Nonprofits I do this in a way that works both for the web and for people.
 </div>
--- pages/home.php
+++ pages/home.php
@@ -2,9 +2,29 @@
 	<inner-column>
 		<!-- <h2 class="intro strong-voice"><?= $pageData["tagline"]?></h2> -->
 		<intro class="intro">
+			<!-- <div data-tilt data-tilt-full-page-listening>
+				<svg class="morph" viewBox="0 0 200 200">
+				  <path id="shape" d="M100,50 A50,50 0 1,0 100.01,50 Z"></path>
+				</svg>
+			</div> -->
+
 			<h2 class="loud-voice intro-headline"><?= $pageData["headline"]?></h2>
 			<p class="strong-voice"><?= $pageData["subheadline"]?></p>
 			<!-- <a href="<?= ENV === 'production'? '/case-study-index' : '?page=case-study-index'?>"><?= $pageData["cta"]?></a> -->
+
+				<svg-container>
+			  <svg viewBox="0 0 200 200">
+			    <path id="shape1" d="M100,50 A50,50 0 1,0 100.01,50 Z"></path>
+			  </svg>
+			  
+			  <svg viewBox="0 0 200 200">
+			    <path id="shape2" d="M100,50 A50,50 0 1,0 100.01,50 Z"></path>
+			  </svg>
+			  
+			  <svg viewBox="0 0 200 200">
+			    <path id="shape3" d="M100,50 A50,50 0 1,0 100.01,50 Z"></path>
+			  </svg>
+			</svg-container>
 		</intro>
 
 		<?php include ('modules/work-cards-home.php'); ?>
