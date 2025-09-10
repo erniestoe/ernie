86c36d7fb81664308d420faba4b05ff94bb5af72
Ernie <ersaavedra.dev@gmail.com>
2025-09-10 09:02:30 -0400

    nav updates
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- modules/site-header.php
+++ modules/site-header.php
@@ -3,19 +3,19 @@
 
 	<ul class="visually-hidden">
 		<li data-tilt data-tilt-scale="1.1">
-			<a class="attention-voice <?= $currentPage['name'] === 'home' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/' : '?page=home'?>">Home</a>
+			<a class="attention-voice <?= $currentPage['name'] === 'home' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/' : '?page=home'?>">Home <div class="menu-circle"></div></a>
 		</li>
 
 		<li data-tilt data-tilt-scale="1.1">
-			<a class="attention-voice <?= $currentPage['name'] === 'design' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/design' : '?page=design'?>">Playground</a>
+			<a class="attention-voice <?= $currentPage['name'] === 'design' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/design' : '?page=design'?>">Playground <div class="menu-circle"></div></a>
 		</li>
 		
-		<li data-tilt data-tilt-scale="1.1">
+		<li data-tilt data-tilt-scale="1.1" class="external-link">
 			<a class="attention-voice" target="_blank" href="https://lapanaderia.substack.com/">Blog</a>
 			<svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#141414" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg>
 		</li>
 
-		<li data-tilt data-tilt-scale="1.1">
+		<li data-tilt data-tilt-scale="1.1" class="external-link">
 			<a class="attention-voice" href="mailto:ersaavedra.nc@gmail.com">Let's Chat</a>
 			<svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#141414" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg>
 		</li>
@@ -26,11 +26,11 @@
 <div class="mobile-menu visually-hidden">
 		<ul>
 			<li>
-				<a class="attention-voice <?= $currentPage['name'] === 'home' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/' : '?page=home'?>">Home</a>
+				<a class="attention-voice <?= $currentPage['name'] === 'home' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/' : '?page=home'?>">Home <div class="menu-circle"></div></a>
 			</li>
 
 			<li>
-				<a class="attention-voice <?= $currentPage['name'] === 'design' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/design' : '?page=design'?>">Playground</a>
+				<a class="attention-voice <?= $currentPage['name'] === 'design' ? 'highlight' : ''?>" href="<?= ENV === 'production'? '/design' : '?page=design'?>">Playground <div class="menu-circle"></div></a>
 			</li>
 			
 			<li>
