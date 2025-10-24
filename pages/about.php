bc4af63a0816d6910be3d2fe19cbb0bb217493a9
Ernie <ersaavedra.dev@gmail.com>
2025-10-24 12:23:40 -0400

    about page update
    Signed-off-by: Ernie <ersaavedra.dev@gmail.com>

--- pages/about.php
+++ pages/about.php
@@ -5,53 +5,45 @@
 				<h1 class="loud-voice">Manifesto de Ernesto</h1>
 			</header>
 
+			<me-card>
+				<picture data-tilt>
+					<img load="lazy" src="https://res.cloudinary.com/dhgciqwbz/image/upload/v1761321202/me-landscape_b2puao.jpg" alt="Picture of Ernie through various stages of life">
+				</picture>
+				<h2 class="quiet-voice bold">The communicator himself.</h2>
+			</me-card>
+
+			<!-- <cta>
+				<p class="attention-voice">I work with people and organizations who believe in substance over spectacle.</p>
+
+				<p class="attention-voice">The ones who constantly think “there has to be a better way to this”, and take action.</p>
+
+				<p class="attention-voice">If you believe in substance over spectacle, or constantly think “there has to be a better way to this”, and take action; <a href="mailto:ersaavedra.nc@gmail.com">let's talk</a>.</p>
+			</cta> -->
+
 			<text>
-				<p>I’ve always had big ideas and something to say, and over some long tireless years, I’ve learned how to turn those ideas into work that moves people.</p>
 
-				<p>To me, design is translation.</p>
+				<p>To me, design is translation. Taking scattered thoughts, half baked messages, and good intentions; and shape them into something that lands. A scroll stopping Reel. A campaign that sparks curiosity and speaks to the right people. A system built to earn trust. All built for clarity and impact. That's it. Not for clicks or trends, and not just to "look pretty". Aesthetics recycle and algorithms rewrite themselves but, strong communication, one that's both intentional and human, always cuts through the noise.</p>
 
-				<p>Taking scattered thoughts, half baked messages, and good intentions; and shape them into something that lands.</p>
+				<p class="attention-voice">If you believe in substance over spectacle, or constantly think “there has to be a better way to this”, and take action; <a href="mailto:ersaavedra.nc@gmail.com">let's talk</a>.</p>
 
-				<p>A scroll stopping Reel.</p>
+				<!-- <p>A scroll stopping Reel. A campaign that sparks curiosity and speaks to the right people. A system built to earn trust.</p>
 
-				<p>A campaign that sparks curiosity and speaks to the right people.</p>
+				<p>All built for clarity and impact. That's it. Not for clicks or trends, and not just to "look pretty".</p>
 
-				<p>A system built to earn trust.</p>
-
-				<p>I design for clarity and impact. That's it.</p>
-
-				<p>Not for clicks or trends, and not just to "look pretty".</p>
-
-				<p>Aesthetics recycle and algorithms rewrite themselves but, strong communication, one that's both intentional and human, always cuts through the noise.</p>
-
-				<p>I work everyday to find that signal.</p>
-
-				<p>The one that builds stories, visuals, and systems that connect ideas to action; To remind people that <em>meaning</em> still matters.</p>
-
-				<p>That doesn't mean I’m endlessly faffing about, on the contrary, I move fast and thrive in chaos. Just ask the food pantry I helped run during the height of the COVID-19 pandemic.</p>
-
-				<p>I’m not a stress junkie by any means, but when the work matters, the pressure’s worth it.</p>
-
-				<p>Simple as that.</p>
-
-				<p>I design to find the way though the fire and into our next golden age, regardless of how short lived it might be.</p>
+				<p>Aesthetics recycle and algorithms rewrite themselves but, strong communication, one that's both intentional and human, always cuts through the noise.</p> -->
 			</text>
+
+			
 		</manifesto>
+
 		
-		<cta>
+		<!-- <cta>
 			<p class="attention-voice">I work with people and organizations who believe in substance over spectacle.</p>
 
 			<p class="attention-voice">The ones who constantly think “there has to be a better way to this”, and take action.</p>
 
 			<p class="attention-voice">If that's you, and I sound like your person, <a href="mailto:ersaavedra.nc@gmail.com">let's talk</a>.</p>
-		</cta>
-
-		<me-card>
-			<picture data-tilt>
-				<img load="lazy" src="https://res.cloudinary.com/dhgciqwbz/image/upload/v1761248794/me_nj2whv.jpg" alt="Picture of Ernie through various stages of life">
-		</picture>
-		<h2 class="quiet-voice bold">The communicator himself.</h2>
-		</me-card>
+		</cta> -->
 		
 	</inner-column>
 </section>
